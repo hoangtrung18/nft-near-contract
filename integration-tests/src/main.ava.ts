4238e3a918d88f393c6193a68b27eb963ffd3f8e
Tony Le <dungle2048@gmail.com>
2023-01-31 16:35:33 +0700

    Implement near nft contract

---
+++ integration-tests/src/main.ava.ts
@@ -0,0 +1,28 @@
+import { Worker, NearAccount } from "near-workspaces";
+import anyTest, { TestFn } from "ava";
+
+const test = anyTest as TestFn<{
+  worker: Worker;
+  accounts: Record<string, NearAccount>;
+}>;
+
+test.beforeEach(async (t) => {
+  // Init the worker and start a Sandbox server
+  const worker = await Worker.init();
+
+  // Deploy contract
+  const root = worker.rootAccount;
+  const contract = await root.createSubAccount("test-account");
+  // Get wasm file path from package.json test script in folder above
+  await contract.deploy(process.argv[2]);
+
+  // Save state for test runs, it is unique for each test
+  t.context.worker = worker;
+  t.context.accounts = { root, contract };
+});
+
+test("default total supply should be eq 0", async (t) => {
+  const { contract } = t.context.accounts;
+  const message: number = await contract.view("nft_total_supply", {});
+  t.is(message, 0);
+});
